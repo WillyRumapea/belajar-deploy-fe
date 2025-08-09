3e485d9375f891e87bedc6f74b70c6cdd8ebfea1
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-09 17:28:07 +0700

    interface regist login

---
+++ public/js/formLogin.js
@@ -0,0 +1,33 @@
+const formLogin = document.getElementById("formLogin");
+const buttonLogin = document.getElementById("buttonLogin");
+
+buttonLogin.addEventListener("click", async (e) => {
+  e.preventDefault();
+  try {
+    const data = {
+      user: formLogin.username.value,
+      password: formLogin.password.value,
+    };
+    const response = await fetch(
+      "https://belajar-deploy-api-production.up.railway.app/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        credentials: "include",
+      }
+    );
+
+    if (response.ok) {
+      alert("berhasil login!");
+      console.log("Login Success!");
+      window.location.href = "../index.html";
+    } else {
+      alert("username atau password salah!");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
---
+++ public/js/formRegist.js
@@ -0,0 +1,32 @@
+const formRegist = document.getElementById("formRegist");
+const buttonRegist = document.getElementById("buttonRegis");
+
+buttonRegist.addEventListener("click", async (e) => {
+  e.preventDefault();
+  try {
+    const data = {
+      user: formRegist.username.value,
+      password: formRegist.password.value,
+    };
+    const response = await fetch(
+      "https://belajar-deploy-api-production.up.railway.app/regist",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        credentials: "include",
+      }
+    );
+
+    if (response.ok) {
+      alert("berhasil regist! silahkan login");
+      console.log("Login Success!");
+    } else {
+      alert("username sudah pernah digunakan! silahkan ganti");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
