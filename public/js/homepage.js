f9722fefd6f830684e36d38775877be562a16c5a
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-12 12:10:47 +0700

    cek login session fe

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,5 +1,20 @@
 const toOrder = document.getElementById("toOrderForm");
 
-toOrder.addEventListener("click", () => {
+toOrder.addEventListener("click", async (e) => {
+  const reqSession = await fetch(
+    "https://belajar-deploy-api-production.up.railway.app/check-session",
+    {
+      credentials: "include",
+    }
+  );
+
+  const data = await reqSession.json();
+
+  if (!data.loggedin) {
+    alert("Silahkan login terlebih dahulu");
+    window.location.href = "./page/formLogin.html";
+    return;
+  }
+
   window.location.href = "page/order.html";
 });
