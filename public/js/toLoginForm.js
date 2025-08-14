bb8a70d0db203ddb97819a8ffa82b013d10e50c1
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-14 10:45:20 +0700

    fix path loginForm

--- public/js/toLoginForm.js
+++ public/js/toLoginForm.js
@@ -2,9 +2,9 @@ const toLoginForm = document.getElementById("toLoginForm");
 const toRegistForm = document.getElementById("toRegistForm");
 
 toLoginForm.addEventListener("click", () => {
-  document.location.href = "./page/formLogin.html";
+  document.location.href = "./page/user/formLogin.html";
 });
 
 toRegistForm.addEventListener("click", () => {
-  document.location.href = "./page/formRegist.html";
+  document.location.href = "./page/user/formRegist.html";
 });
