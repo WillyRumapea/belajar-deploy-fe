00968df6c6319698b2bf1686d6e9029e2d51229c
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-15 12:17:58 +0700

    admin dashboard admin2

--- public/js/admin/fetchData.js
+++ public/js/admin/fetchData.js
@@ -10,7 +10,26 @@ const fetchData = async () => {
 
     const data = await reqData.json();
     console.log(data);
+
     const tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+
+    data.forEach((item) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+            <td style="border: 1px solid black; padding: 8px;">${item.id_makanan}</td>
+            <td style="border: 1px solid black; padding: 8px;">${item.nama_makanan}</td>
+            <td style="border: 1px solid black; padding: 8px;">${item.harga_makanan}</td>
+            <td style="border: 1px solid black; padding: 8px;">${item.gambar_makanan}</td>
+            <div>
+                <td>
+                    <button style="background-color: #ffed29">Update!</button>
+                    <button style="background-color: #ff1919>Delete</button>
+                </td>
+            </div>
+        `;
+      tbody.appendChild(row);
+    });
   } catch (err) {
     console.log(err);
   }
