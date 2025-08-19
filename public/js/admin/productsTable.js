cd604abcb8224612b46741f6de30e2c0360179ec
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-19 19:57:20 +0700

    test fix prods table 3

--- public/js/admin/productsTable.js
+++ public/js/admin/productsTable.js
@@ -9,7 +9,7 @@ const fetchDataProds = async () => {
 
   const data = await reqDataProds.json();
   console.log(data);
-  const dataUsers = data[0];
+  const dataUsers = await data.json();
   console.log(dataUsers);
 
   const tbody = document.getElementsByClassName("tbody")[0];
