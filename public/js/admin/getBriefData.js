d4fcb90526053291abe80576fd00c030a76c46aa
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-19 20:19:27 +0700

    test fix dashboard 2

--- public/js/admin/getBriefData.js
+++ public/js/admin/getBriefData.js
@@ -1,41 +1,64 @@
-const infoBrief = document.querySelector(".brief-info");
+const briefInfo = document.querySelector(".brief-info");
+const dataUser = async () => {
+  try {
+    const reqDataUsers = await fetch(
+      "https://belajar-deploy-api-production.up.railway.app/daftar-users"
+    );
 
-async function loadPage(page) {
-  if (page === "dashboard") {
-    return;
-  } else if (page === "users") {
-    try {
-      const res = await fetch("./userTable.html");
-      const html = await res.text();
-
-      infoBrief.innerHTML = html;
-
-      const script = document.createElement("script");
-      script.setAttribute("src", "./js/userTable.js");
-      document.body.appendChild(script);
-    } catch (err) {
-      infoBrief.innerHTML = `<p style="color:red;">Error load users: ${err.message}</p>`;
+    if (!reqDataUsers.ok) {
+      throw new Error(`HTTP error! Status: ${reqDataUsers.status}`);
     }
-  } else if (page === "products") {
-    try {
-      const res = await fetch("./productsTable.html");
-      const html = await res.text();
 
-      infoBrief.innerHTML = html;
+    const data = await reqDataUsers.json();
+    const dataUsers = data.users;
+    const amountUsers = dataUsers.length;
+    console.log("Total users:", amountUsers);
 
-      const script = document.createElement("script");
-      script.setAttribute("src", "./js/productsTable.js");
-      document.body.appendChild(script);
-    } catch (err) {
-      infoBrief.innerHTML = `<p style="color:red;">Error load products: ${err.message}</p>`;
-    }
+    const info = document.createElement("div");
+    const infoTitle = document.createElement("h3");
+    const infoText = document.createElement("p");
+
+    infoTitle.textContent = "User";
+    infoText.textContent = amountUsers;
+
+    info.appendChild(infoText);
+    info.appendChild(infoTitle);
+    briefInfo.appendChild(info);
+  } catch (err) {
+    console.log("Error fetch data", err);
   }
-}
+};
 
-document.querySelectorAll(".navLink").forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const page = link.getAttribute("data-page");
-    loadPage(page);
-  });
-});
+const dataProds = async () => {
+  try {
+    const reqDataProds = await fetch(
+      "https://belajar-deploy-api-production.up.railway.app/daftar-makanan"
+    );
+
+    if (!reqDataProds.ok) {
+      throw new Error(`HTTP error, Status: ${reqDataProds.status}`);
+    }
+
+    const dataProds = await reqDataProds.json();
+    const amountProds = dataProds.length;
+    console.log("Total products:", amountProds);
+
+    const info = document.createElement("div");
+    const infoTitle = document.createElement("h3");
+    const infoText = document.createElement("p");
+
+    infoTitle.textContent = "Products";
+    infoText.textContent = amountProds;
+
+    info.appendChild(infoText);
+    info.appendChild(infoTitle);
+    briefInfo.appendChild(info);
+  } catch (err) {
+    console.log("Error fetch data products", er);
+  }
+};
+
+briefInfo.innerHTML = "";
+
+dataUser();
+dataProds();
