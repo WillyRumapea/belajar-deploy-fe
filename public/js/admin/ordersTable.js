75e1fcef4354f7c9c84da126d5ef46f0745e6413
WillyRumapea <willyrumapea123jr@gmail.com>
2025-09-05 00:06:32 +0700

    table orders admin

---
+++ public/js/admin/ordersTable.js
@@ -0,0 +1,37 @@
+const fetchDataOrders = async () => {
+  const reqDataOrders = await fetch(
+    "https://belajar-deploy-api-production.up.railway.app/daftar-pesanan"
+  );
+
+  if (!reqDataOrders.ok) {
+    throw new Error(`HTTP error! Status: ${reqDataOrders.status}`);
+  }
+
+  const dataJson = await reqDataProds.json();
+  const dataOrders = dataJson.data;
+
+  const tbody = document.getElementsByClassName("tbody")[0];
+
+  tbody.innerHTML = "";
+
+  dataOrders.forEach((item) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td style="border: 1px solid black; padding: 8px;">${item.orders_id}</td>
+        <td style="border: 1px solid black; padding: 8px;">${item.orders_customer}</td>
+        <td style="border: 1px solid black; padding: 8px;">${item.orders_menu}</td>
+        <td style="border: 1px solid black; padding: 8px;">${item.orders_amount}</td>
+        <td style="border: 1px solid black; padding: 8px;">${item.orders_total_price}</td>
+        <td style="border: 1px solid black; padding: 8px;">${item.orders_status}</td>
+        <td style="border: 1px solid black; padding: 8px;">
+            <div>
+                <button>update</button>
+                <button>hapus</button>
+            </div>
+        </td>
+    `;
+    tbody.appendChild(row);
+  });
+};
+
+fetchDataOrders();
