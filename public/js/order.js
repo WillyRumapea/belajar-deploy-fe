87ff3950a7913bf38f246cb027fadf9a4ad0d5a3
WillyRumapea <willyrumapea123jr@gmail.com>
2025-09-17 01:07:57 +0700

    style order page31

--- public/js/order.js
+++ public/js/order.js
@@ -67,7 +67,7 @@ const getDataFetch = async () => {
             "items-center",
             "justify-center",
             "border-2",
-            "w-[80%]"
+            "w-[100%]"
           );
           const btnMin = document.createElement("button");
           btnMin.classList.add(
