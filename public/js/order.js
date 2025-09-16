4681be1e7942eb5466759cdd944302a594c7cf1e
WillyRumapea <willyrumapea123jr@gmail.com>
2025-09-17 00:26:34 +0700

    style order page5

--- public/js/order.js
+++ public/js/order.js
@@ -37,7 +37,7 @@ const getDataFetch = async () => {
       "border-white",
       "p-2",
       "rounded-lg",
-      "w-[200px]",
+      "w-[60%]",
       "border-2",
       "border-crimson"
     );
