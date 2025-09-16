aa3c0b9f0e5b47333874125b762b039f2ad63fa0
WillyRumapea <willyrumapea123jr@gmail.com>
2025-09-16 10:56:38 +0700

    style untuk homepage

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+      },
+      colors: {
+        bisque: "#FFEABF",
+        cornsilk: "#fff5d9",
+        earthyBrown: "#943a2c",
+        crimson: "#c24319",
+        floralWhite: "#fffcf5",
+      },
+      fontWeight: {
+        normal: 500,
+        bold: 600,
+        bolder: 700,
+      },
+    },
+  },
+  plugins: [],
+};
