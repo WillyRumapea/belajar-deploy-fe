9e3566765d267ca8f797726802d381ab4456745f
WillyRumapea <willyrumapea123jr@gmail.com>
2025-09-17 14:55:51 +0700

    style untuk delivery dan perbaikan form login

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         floralWhite: "#fffcf5",
         paleYellow: "#fffceb",
         gilled: "#de5418",
+        lightOrange: "#ffe08c",
       },
       fontWeight: {
         normal: 500,
